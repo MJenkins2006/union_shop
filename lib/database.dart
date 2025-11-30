90ca7df32e1012c0b9e2998449171ef39eeffd3c
Matthew Jenkins <matthew.t.jenkins9@gmail.com>
2025-11-30 11:53:01 +0000

    Create database.dart

---
+++ lib/database.dart
@@ -0,0 +1,56 @@
+  final List<Map<String, String>> collections = [
+    {
+      'title': 'Sales',
+      'imageUrl':
+          'https://shop.upsu.net/cdn/shop/files/Pink_Essential_Hoodie_2a3589c2-096f-479f-ac60-d41e8a853d04_720x.jpg?v=1749131089',
+    },
+    {
+      'title': 'Cards',
+      'imageUrl':
+          'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+    },
+    {
+      'title': 'Clothes',
+      'imageUrl':
+          'https://shop.upsu.net/cdn/shop/files/Signature_T-Shirt_Indigo_Blue_2_1080x.jpg?v=1758290534',
+    },
+  ];
+
+  final List<Map<String, String>> products = [
+    {
+      'collection': 'Sales',
+      'title': 'Hoodies',
+      'imageUrl':
+          'https://shop.upsu.net/cdn/shop/files/Pink_Essential_Hoodie_2a3589c2-096f-479f-ac60-d41e8a853d04_720x.jpg?v=1749131089',
+    },
+    {
+      'collection': 'Sales',
+      'title': 'Shirts',
+      'imageUrl':
+          'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+    },
+    {
+      'collection': 'Cards',
+      'title': 'Postcard',
+      'imageUrl':
+          'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+    },
+    {
+      'collection': 'Cards',
+      'title': 'Card',
+      'imageUrl':
+          'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+    },
+    {
+      'collection': 'Clothes',
+      'title': 'Hoodies',
+      'imageUrl':
+          'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+    },
+    {
+      'collection': 'Clothes',
+      'title': 'Shirts',
+      'imageUrl':
+          'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+    },
+  ];
