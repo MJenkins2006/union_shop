fda31bc5517fb33d5f680f70a9f12a754745a99b
Matthew Jenkins <matthew.t.jenkins9@gmail.com>
2025-11-30 15:37:52 +0000

    Add ProductCard to common_widgets

--- lib/views/common_widgets.dart
+++ lib/views/common_widgets.dart
@@ -571,3 +571,87 @@ Widget buildFooter(BuildContext context) {
   }
   return buildFooterMobile(context);
 }
+
+class ProductCard extends StatelessWidget {
+  final String title;
+  final String price;
+  final String imageUrl;
+
+  const ProductCard({
+    super.key,
+    required this.title,
+    required this.price,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/product');
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Image.network(
+              imageUrl,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return Container(
+                  color: Colors.grey[300],
+                  child: const Center(
+                    child: Icon(Icons.image_not_supported, color: Colors.grey),
+                  ),
+                );
+              },
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 4),
+              Text(
+                title,
+                style: const TextStyle(fontSize: 14, color: Colors.black),
+                maxLines: 2,
+              ),
+              const SizedBox(height: 4),
+              // Strikethrough on multiple values
+              Builder(builder: (context) {
+                final prices = price.split(' ');
+                String oldPrices = '';
+                for (var i = 0; i < prices.length - 1; i++) {
+                  oldPrices += '${prices[i]} ';
+                }
+                if (prices.length != 1) {
+                  return Row(
+                    children: [
+                      Text(
+                        oldPrices,
+                        style: 
+                          const TextStyle(fontSize: 13, color: Colors.grey,
+                          decoration: TextDecoration.lineThrough,
+                        ),
+                      ),
+                      Text(
+                        prices[prices.length - 1],
+                        style:
+                            const TextStyle(fontSize: 13, color: Colors.grey),
+                      ),
+                    ],
+                  );
+                } else {
+                  return Text(
+                    price,
+                    style: const TextStyle(fontSize: 13, color: Colors.grey),
+                  );
+                }
+              }),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
