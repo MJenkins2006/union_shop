2c0405cb3c8004c448d81dd4a3f2ac6ef4e9fe6f
Matthew Jenkins <matthew.t.jenkins9@gmail.com>
2025-11-30 13:33:26 +0000

    Restructure database.dart

--- lib/database.dart
+++ lib/database.dart
@@ -1,16 +1,16 @@
   final List<Map<String, String>> collections = [
     {
-      'title': 'Sales',
+      'collection': 'SALES',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/Pink_Essential_Hoodie_2a3589c2-096f-479f-ac60-d41e8a853d04_720x.jpg?v=1749131089',
     },
     {
-      'title': 'Cards',
+      'collection': 'CARDS',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
     },
     {
-      'title': 'Clothes',
+      'collection': 'CLOTHES',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/Signature_T-Shirt_Indigo_Blue_2_1080x.jpg?v=1758290534',
     },
@@ -18,37 +18,37 @@
 
   final List<Map<String, String>> products = [
     {
-      'collection': 'Sales',
+      'collection': 'SALES',
       'title': 'Hoodies',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/Pink_Essential_Hoodie_2a3589c2-096f-479f-ac60-d41e8a853d04_720x.jpg?v=1749131089',
     },
     {
-      'collection': 'Sales',
+      'collection': 'SALES',
       'title': 'Shirts',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
     },
     {
-      'collection': 'Cards',
+      'collection': 'CARDS',
       'title': 'Postcard',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
     },
     {
-      'collection': 'Cards',
+      'collection': 'CARDS',
       'title': 'Card',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
     },
     {
-      'collection': 'Clothes',
+      'collection': 'CLOTHES',
       'title': 'Hoodies',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
     },
     {
-      'collection': 'Clothes',
+      'collection': 'CLOTHES',
       'title': 'Shirts',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
