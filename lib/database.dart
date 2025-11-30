d8c6923c898bfb49b88ac9e504849dde990e27a4
Matthew Jenkins <matthew.t.jenkins9@gmail.com>
2025-11-30 15:32:37 +0000

    Add pricing to products

--- lib/database.dart
+++ lib/database.dart
@@ -19,37 +19,57 @@
   final List<Map<String, String>> products = [
     {
       'collection': 'SALES',
-      'title': 'Hoodies',
+      'title': 'Limited Edition Essential Zip Hoodies',
+      'price': '20.00 14.99',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/Pink_Essential_Hoodie_2a3589c2-096f-479f-ac60-d41e8a853d04_720x.jpg?v=1749131089',
     },
     {
       'collection': 'SALES',
-      'title': 'Shirts',
+      'title': 'Essential T-Shirts',
+      'price': '10.00 6.99',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
     },
     {
       'collection': 'CARDS',
       'title': 'Postcard',
+      'price': '2.00',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
     },
     {
       'collection': 'CARDS',
       'title': 'Card',
+      'price': '2.00',
+      'imageUrl':
+          'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+    },
+    {
+      'collection': 'CARDS',
+      'title': 'Bookmark',
+      'price': '3.00',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
     },
     {
       'collection': 'CLOTHES',
       'title': 'Hoodies',
+      'price': '14.99',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
     },
     {
       'collection': 'CLOTHES',
       'title': 'Shirts',
+      'price': '12.99',
+      'imageUrl':
+          'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+    },
+    {
+      'collection': 'CLOTHES',
+      'title': 'Trousers',
+      'price': '9.99',
       'imageUrl':
           'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
     },
